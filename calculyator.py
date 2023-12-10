20b38b9d83a9bf664ee1acfd4fa8f0178cafdee4
calkulate1 <144926743+calkulate1@users.noreply.github.com>
2023-12-10 20:39:47 +0300

    Bank.py

--- calculyator.py
+++ calculyator.py
@@ -1,17 +1,6 @@
-print('calucale')
-a = int(input('введите число '))
-b = int(input('введите число '))
-c = input('введите символ / +  - * ** ')
-if b == 0 and c == '/':
-    print('ОШИБКА НА 0 ДЕЛИТЬ НЕЛЬЗЯ')
-if c == '+':
-    print('a', '+', 'b', '= ', a + b )
-if c == '-':
-    print('a', '-', 'b', '= ', a - b)
-if c == '/' and b != 0:
-    print('a', '/', 'b', '= ', a / b )
-if c == '*':
-    print('a', '*', 'b', '= ', a * b )
-if c == '+':
-    print('a', '**', 'b', '= ', a ** b )
+def bank(many, years, percent):
+    a = many * (1 + (percent / 100) * years)
+    print(f'в итоге вам придется выплотить...\n{a}')
 
+
+bank(float(input('сколько денег вы берете... \n')), float(input('на сколько лет... \n')), float(input('под сколько процентов... \n')))
